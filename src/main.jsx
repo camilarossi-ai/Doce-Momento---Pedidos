4fdfe633318ab874bc07e0b195420f083f00a052
camilarossi-ai <camilarossi0810@gmail.com>
2026-08-11 21:01:36 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Painel from './Painel.jsx'
 import './index.css'
+
+const isPainel = window.location.pathname.replace(/\/$/, '') === '/painel'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {isPainel ? <Painel /> : <App />}
   </React.StrictMode>,
 )
